3174ba7336ffb15541a8006a479567aa759052c5
VDeshmukhCemtrex <vdeshmukh@cxr.agency>
2023-09-12 19:05:54 +0530

    Storyboard UI IBOutlets Storyboard Logic Storyboard Buttons UI - Setup UIButtons

---
+++ CardWorkoutApp/CardWorkoutApp/CardSelectionViewController.swift
@@ -0,0 +1,31 @@
+//
+//  CardSelectionViewController.swift
+//  CardWorkoutApp
+//
+//  Created by CEMTREX on 12/09/23.
+//
+
+import UIKit
+
+class CardSelectionViewController: UIViewController {
+    
+    @IBOutlet weak var cardImageView: UIImageView!
+    
+    @IBOutlet var buttons: [UIButton]!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+    @IBAction func stopButtonTapped(_ sender: Any) {
+   
+    }
+    
+    @IBAction func restartButtonTapped(_ sender: Any) {
+    }
+    
+    @IBAction func rulesButtonTapped(_ sender: Any) {
+    }
+}
