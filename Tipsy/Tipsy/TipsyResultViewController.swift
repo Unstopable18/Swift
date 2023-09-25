0bdba822f6b74822cbb4904683af5c7dea51c3e9
VDeshmukhCemtrex <vdeshmukh@cxr.agency>
2023-09-25 18:43:40 +0530

    Added Stepper and Button states

---
+++ Tipsy/Tipsy/TipsyResultViewController.swift
@@ -0,0 +1,18 @@
+//
+//  TipsyResultViewController.swift
+//  Tipsy
+//
+//  Created by CEMTREX on 25/09/23.
+//
+
+import UIKit
+
+class TipsyResultViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+}
