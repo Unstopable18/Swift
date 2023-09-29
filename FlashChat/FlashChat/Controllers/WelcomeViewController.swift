6c28baee72084e1a5ce41cd486a412c9bbf0431b
VDeshmukhCemtrex <vdeshmukh@cxr.agency>
2023-09-29 18:30:24 +0530

    Cocoapods CLTyping pod added

--- FlashChat/FlashChat/Controllers/WelcomeViewController.swift
+++ FlashChat/FlashChat/Controllers/WelcomeViewController.swift
@@ -6,23 +6,16 @@
 //
 
 import UIKit
+import CLTypingLabel
 
 class WelcomeViewController: UIViewController {
 
-    @IBOutlet weak var titleLabel: UILabel!
+    @IBOutlet weak var titleLabel: CLTypingLabel!
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        titleLabel.text=""
-        let titleText="⚡️FlashChat"
+        titleLabel.text="⚡️FlashChat"
         
-        var index=0.0
-        for letter in titleText{
-            Timer.scheduledTimer(withTimeInterval: 0.1*index, repeats: false){(timer) in
-                self.titleLabel.text?.append(letter)
-            }
-            index+=1
-        }
     }
     
 }
